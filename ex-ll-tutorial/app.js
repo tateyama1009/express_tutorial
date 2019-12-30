2671f6daaf1d8277473bce5a562e13580655d377
tateyama1009 <1416070@ed.tus.ac.jp>
2019-12-31 00:16:48 +0900

    a

--- ex-ll-tutorial/app.js
+++ ex-ll-tutorial/app.js
@@ -4,12 +4,19 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var helmet = require('helmet');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var catalogRouter = require('./routes/catalog');
 //Import routes for "catalog" area of site
 
+var compression = require('compression');
+
+//Create the Express application object
 var app = express();
+
+app.use(helmet());
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
@@ -25,6 +32,8 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+app.use(compression());//compression all routes
 
 app.use(logger('dev'));
 app.use(express.json());
